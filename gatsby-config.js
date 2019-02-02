6370c0ea7434aca0f67aa08dc7c0ad9f68e80bd7
kalwalt <info@kalwaltart.it>
2019-02-02 14:50:07 +0100

    added gatsby-plugin-manifest and -offline, and coffe-logo.png

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,22 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `static/img/coffe-logo.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      },
+    },
+    'gatsby-plugin-offline',
+    {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {

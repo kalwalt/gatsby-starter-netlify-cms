9f119f1078cca18656d737352d1856737d5cba87
kalwalt <info@kalwaltart.it>
2019-03-03 12:36:23 +0100

    deleting not necessary code

--- src/components/en.js
+++ src/components/en.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Layout from './index';
 import Layout from '../components/Layout'
 import { addLocaleData } from 'react-intl';
 
--- src/components/it.js
+++ src/components/it.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Layout from './index';
 import Layout from 'Layout'
 import { addLocaleData } from 'react-intl';
 
